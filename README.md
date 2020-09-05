2ec3b5fcd5a8d6112befd767ea16fdbd5cc5f970
Kartik Goel <goel.kartik39@gmail.com>
2020-09-05 14:19:26 +0530

    Initialized server

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Project-eUreka
+init website
---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+//Requiring routes
+const patientAuth = require("./routes/patientAuth");
+const doctorAuth = require("./routes/doctorAuth");
+const prediction = require("./routes/prediction")
+const prescription = require("./routes/prescription");
+const appointment = require("./routes/appointment");
+
+const db = process.env.MONGODB_URL;
+
+mongoose.connect(db, {useNewUrlParser : true,
+    useUnifiedTopology: true,
+    useCreateIndex : true})
+.then(() => console.log('Database connected'))
+.catch((err) => {
+    console.log(err);
+})
+
+app.use(express.urlencoded({
+    extended : false
+}));
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+//Definning routes
+app.use("/patient",patientAuth);
+app.use("/doctor",doctorAuth);
+app.use("/predict",prediction);
+app.use("/prescription",prescription);
+app.use("/appointment",appointment);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+});
